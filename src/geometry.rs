8bc1e1e1d7b977c090382fcab46258be45550c77
Stanislav Bas <stanislav.m.bas@gmail.com>
2019-01-20 21:47:06 +0200

    Implement basic Ray type

--- src/geometry.rs
+++ src/geometry.rs
@@ -126,6 +126,22 @@ impl DivAssign<f64> for Vector3 {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct Ray {
+    pub origin: Vector3,
+    pub direction: Vector3,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3, direction: Vector3) -> Ray {
+        Ray {origin, direction: direction.normalized()}
+    }
+
+    pub fn point_at(&self, t: f64) -> Vector3 {
+        self.origin + self.direction * t
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
