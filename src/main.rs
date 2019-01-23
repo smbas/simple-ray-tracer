2689a238b95093df9cc1935b54193e8cdde4e829
Stanislav Bas <stanislav.m.bas@gmail.com>
2019-01-23 20:43:44 +0200

    Add many spheres

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,16 @@ fn main() {
 
     println!("P3\n{} {}\n255", nx, ny);
 
-    let mut world = HitWorld::new();
-    world.add(Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5, Lambertian::new(Vector3::new(0.8, 0.3, 0.3))));
-    world.add(Sphere::new(Vector3::new(0.0, -100.5, -1.0), 100.0, Lambertian::new(Vector3::new(0.8, 0.8, 0.0))));
-    world.add(Sphere::new(Vector3::new(1.0, 0.0, -1.0), 0.5, Metal::new(Vector3::new(0.8, 0.6, 0.2), 0.3)));
-    world.add(Sphere::new(Vector3::new(-1.0, 0.0, -1.0), 0.5, Dielectric::new(1.5)));
-
-    let lookfrom = Vector3::new(3.0, 3.0, 2.0);
-    let lookat = Vector3::new(0.0, 0.0, -1.0);
+    let world = random_scene();
+    let lookfrom = Vector3::new(8.0, 2.0, 3.0);
+    let lookat = Vector3::new(4.0, 1.0, 2.0);
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vector3::new(0.0, 1.0, 0.0),
         60.0,
         (nx as f64) / (ny as f64),
-        0.4,
+        0.1,
         (lookat - lookfrom).length(),
     );
 
@@ -53,4 +48,51 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+}
+
+fn random_scene<'a>() -> HitWorld<'a> {
+    let mut world = HitWorld::new();
+    world.add(Sphere::new(Vector3::new(0.0, -1000.0, 0.0), 1000.0, Lambertian::new(Vector3::new(0.5, 0.5, 0.5))));
+    let mut rng = rand::thread_rng();
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let mat: f64 = rng.gen();
+            let center = Vector3::new(a as f64 + 0.9 * rng.gen::<f64>(), 0.2, b as f64 + 0.9 * rng.gen::<f64>());
+
+            if (center - Vector3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if mat < 0.8 {
+                    world.add(Sphere::new(
+                        center,
+                        0.2,
+                        Lambertian::new(Vector3::new(
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>(),
+                            rng.gen::<f64>() * rng.gen::<f64>()
+                        ))
+                    ));
+                } else if mat < 0.95 {
+                    world.add(Sphere::new(
+                        center,
+                        0.2,
+                        Metal::new(Vector3::new(
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                                0.5 * (1.0 + rng.gen::<f64>()),
+                            ),
+                            0.5 * rng.gen::<f64>()
+                        )
+                    ));
+                } else {
+                    world.add(Sphere::new(center, 0.2, Dielectric::new(1.5)));
+                }
+            }
+        }
+    }
+
+    world.add(Sphere::new(Vector3::new(0.0, 1.0, 0.0), 1.0, Dielectric::new(1.5)));
+    world.add(Sphere::new(Vector3::new(-4.0, 1.0, 0.0), 1.0, Lambertian::new(Vector3::new(0.4, 0.2, 0.1))));
+    world.add(Sphere::new(Vector3::new(4.0, 1.0, 0.0), 1.0, Metal::new(Vector3::new(0.7, 0.6, 0.5), 0.0)));
+
+    world
 }
